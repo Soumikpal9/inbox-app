54f3447b5438c973aab775f11b3b1605cb6c112b
Pal <soumik.pal@capgemini.com>
2022-03-04 04:26:16 +0530

    [SP]: Login with the help of Github implemented

--- src/main/java/com/kitz/messaging/InboxAppApplication.java
+++ src/main/java/com/kitz/messaging/InboxAppApplication.java
@@ -7,21 +7,38 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.kitz.connection.DataStaxAstraProperties;
 
 @SpringBootApplication
 @EnableConfigurationProperties(DataStaxAstraProperties.class)
+@RestController
 public class InboxAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InboxAppApplication.class, args);
 	}
 	
+	
+	/**
+	 * 
+	 * This is necessary to have the Spring Boot app use the astra secure bundle
+	 * to connect to the database
+	 */
 	@Bean
 	public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties) {
 		Path bundle = astraProperties.getSecureConnectBundle().toPath();
 		return builder -> builder.withCloudSecureConnectBundle(bundle);
+	}
+	
+	@RequestMapping("/user")
+	public String user(@AuthenticationPrincipal OAuth2User principal) {
+		System.out.println(principal);
+		return principal.getAttribute("name");
 	}
 
 }
